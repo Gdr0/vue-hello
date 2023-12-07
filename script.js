9eeecb15abda38f5783e08153dee0af85aa1b315
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2023-12-07 16:59:35 +0100

    completo con bonus

---
+++ script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      messaggio: "So Anyway,",
+      textPink: "pink",
+      danny: "img/soanyway.jpg",
+    };
+  },
+}).mount("#app");
